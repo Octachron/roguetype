ae08924c86f6b12a68216a93f0734f13dcd65735
octachron <octa@polychoron.fr>
2020-03-24 22:29:29 +0100

    The time of the second dimension has come

--- main.ml
+++ main.ml
@@ -1,154 +1,331 @@
+type  'a zip = 'a constraint
+  'a = <l:'l; m:'m; r:'r>
 
-type empty = Empty
-type key = Key
-type _ free =
-  | Empty: empty free
-  | Key: key free
-type door = Door
-type wall = Wall
-type stair = Stair
+type 'a h = 'a -> 'a -> 'a -> 'a -> 'a -> 'a
 
-type 'p player = <left_hand:'a; right_hand:'b>
-  constraint 'p = 'a * 'b
+type 'p col = 'p constraint
+  'p = <
+    l:'a -> 'b -> 'c -> 'd -> 'e -> 'f;
+    m:'mid;
+    r: 'r1 -> 'r2 -> 'r3 -> 'r4 -> 'r5 ->'r6
+  > zip
 
-type 'a case = 'a
-  constraint 'a = <now:_; after: _; back: _ >
+type 'p cols = 'p constraint
+  'p = <
+    l:'l1 col -> 'l2 col -> 'l3 col -> 'l4 col -> 'l5 col -> 'l6 col;
+    m:'mid col;
+    r: 'e col -> 'f col -> 'g col -> 'h col -> 'r5 col -> 'r6 col
+  > zip
 
-type 'a hcase = <now:'a; after:'a; back:'a> case
+type true'
+type false'
 
-type cdoor = <now:door free; after:wall; back:door free>
-
-type ('a,'b) state = <self: 'a player; case: 'b free >
-
-type _ case_builder =
-  | Case: empty free hcase case_builder
-  | Key: key free hcase case_builder
-  | Door: cdoor case_builder
-  | Stair: stair free hcase case_builder
-
-type 'a board = 'a
-    constraint 'a = < left:'l; player:'p player; at:'c case; right: 'r >
-
-type e = empty free
-type k = key free
-type d = door
-
-module type T = sig type t end
-type 'a ty = (module T with type t = 'a)
-type 'a tyw = Ty
-
-module Builder = struct
-  type _ t =
-    | []: e t
-    | (::): 'a case_builder * 'b t -> ('a -> 'b) t
-
-
-  let typeOf (type x) (_: x tyw): x ty =
-    (module struct type t = x end)
-  let start ~left:(x:'a t) ~right:(y:'b t) =
-    typeOf(Ty: <left:'a; at: empty free hcase; right:'b; player:(empty * empty) player> tyw)
+module Inventory = struct
+  type empty = Empty
+  type 'a pickable = P
+  type key = Key
 end
 
-let left = Builder.[Case;Door; Key;Key;Case;Case]
-let right = Builder.[Case; Key; Door; Door; Stair;Case]
-module Start = (val Builder.start ~left ~right)
+module Previous = struct
+  type left = Left
+  type right = Right
+  type down = Down
+  type up = Up
+  type none = None
+end
 
-type 'a stop = <right: e; .. >  as 'a
-type 'arg right =
-  <
-    left:'at -> <now:'l1; after:'la2; back:'l1> -> 'l
-  ; player:'p
-  ; at: <back:'back free; now:'back free; after:'after>
-  ; right:<now:'after; back:'r2; after:'a2> -> 'r3
-  > board
-  constraint 'arg = <
-    left:<back:'l1; after:'la2; ..> -> 'l;
-    player:'p;
-    at:'at;
-    right:<now:'r free;after:'after; back:'back free> -> <now:'r2; after:'a2; ..> -> 'r3
-  > board
 
-type 'arg left =
-  <left: <now:'la; back:'l2; after: 'l2a> -> 'l3;
-   at:  <back:'back free; now:'back free; after:'la>;
-   right:'at -> <now:'r1b; back:'r1b; after:'r1a> -> 'r2;
-   player:'p
-  > board
-  constraint 'arg =
-    <left: <now:'l free; after:'la; back:'back free> -> <now:'l2; back:_; after:'l2a> -> 'l3;
-     at:'at;
-     right: <now:_; back:'r1b; after:'r1a> -> 'r2;
-     player:'p;
-    > board
+module Events = struct
+  type door = Door
+  type stair = Stair
+end
 
-type 'arg take =
-  <
-    left:'l;
-    player: <left_hand:'on_floor; right_hand:'lh>;
-    right:'r;
-    at: 'rh free hcase
-  >
-constraint 'arg =  <
-    left:'l;
-    player: <left_hand:'lh; right_hand:'rh>;
-    right:'r;
-    at: 'on_floor free hcase
-  >
+type border = <event:false'; free:false'; floor:Inventory.empty>
+type free = <event:false'; free:true'; floor:Inventory.empty>
+type key = <event:false'; free:true'; floor: Inventory.key Inventory.pickable>
+type stair = <free:true';  event:Events.stair; floor:Inventory.empty>
+type door = <event: Events.door; free:true'; stair:false'; floor: Inventory.key Inventory.pickable>
 
-type 'arg open_door =
-  <left: <now:'lb; after:'la; back:'lb> -> 'l;
-   player: <left_hand:empty; right_hand: 'rh>;
-   right: <now:'rb; after:'ra; back:'rb> -> 'r;
-   at: e hcase >
-constraint 'arg = <
-  left: <back:'lb; after:'la; ..> -> 'l;
-  player: <left_hand:key; right_hand:'rh>;
-  right: <back:'rb; after:'ra; ..>  -> 'r;
-  at: <now:door free; ..>
+
+type f = free
+type b = border
+
+type won = Won
+
+
+type 'a hzip = <
+  l: 'a h;
+  m: 'a;
+  r: 'a h
+> col
+
+type ('a,'b) state =
+  < world:'a; player:'b>
+
+
+
+type player_start = < left_hand:Inventory.empty; right_hand:Inventory.empty; back:Previous.none >
+
+module Builder = struct
+  type _ elt =
+    | F: free elt
+    | W: border elt
+    | S: stair elt
+    | K: key elt
+    | D: door elt
+  type 'a l =
+    | []: (border -> border -> border) l
+    | (::): 'a elt * 'b l -> ('a -> 'b) l
+
+  type 'p inv = 'c -> ' b -> 'a -> 'e -> 'f -> 'g
+    constraint 'p = 'a -> 'b -> 'c -> 'e -> 'f -> 'g
+
+  type 'p bcol = 'l inv l * 'm elt * 'r l
+    constraint 'p = <l:'l; m:'m; r:'r >
+
+
+
+  type 'a ll =
+    | []: (border hzip -> border hzip -> border hzip) ll
+    | (::): 'p bcol * 'b ll -> ('p -> 'b) ll
+
+  type 'p bcols = 'l inv ll * 'm bcol * 'r ll
+    constraint 'p = <l:'l; m:'m; r:'r >
+
+end
+
+
+
+type 'p first = 'a
+  constraint 'p = 'a -> 'b -> 'c -> 'd -> 'e -> 'f
+
+type ('p,'x) app = 'a -> 'b -> 'c -> 'd-> 'e -> 'x
+  constraint 'p = 'a -> 'b -> 'c -> 'd -> 'e -> 'f
+
+
+type ('p,'x) pull = 'b -> 'c -> 'd -> 'e -> 'f -> 'x
+  constraint 'p = 'a -> 'b -> 'c -> 'd -> 'e -> 'f
+
+type ('x,'p) push = 'x -> 'a -> 'b -> 'c -> 'd -> 'e
+  constraint 'p = 'a -> 'b -> 'c -> 'd -> 'e -> 'f
+
+
+
+type 'a is_free = 'a constraint 'a = <free:true'; ..>
+type 'a mfree = < m: <free:true'; ..> ; ..> as 'a
+
+type 'p l = 'l
+  constraint 'p = <l:'l; .. >
+
+type 'p r = 'r
+  constraint 'p = <r:'r; .. >
+
+type 'p m = 'm
+  constraint 'p = <m:'m; .. >
+
+
+type 'p cup = <
+  l: ('p l,b) pull;
+  m:'p l first;
+  r: ('p m, 'p r) push
 >
 
-type 'arg swap =
-  <
-    left:'l;
-    player: <left_hand:'rh; right_hand:'lh>;
-    right:'r;
-    at: 'at
+type 'p cdw =
+  <l: ('p m, 'p l) push;
+   m:'p r first;
+   r: ('p r, b) pull >
+
+type 'p lcup = 'a cup -> 'b cup -> 'c cup -> 'd cup -> 'e cup -> 'f cup
+    constraint 'p = 'a -> 'b -> 'c -> 'd -> 'e -> 'f
+
+type 'p lcdw = 'a cdw -> 'b cdw -> 'c cdw -> 'd cdw  -> 'e cdw -> 'f cdw
+    constraint 'p = 'a -> 'b -> 'c -> 'd -> 'e -> 'f
+
+
+
+type 'p up = <
+  l: 'p l lcup;
+  m: 'p m cup mfree;
+  r: 'p r lcup
+  > cols
+
+type 'p dw = <
+  l: 'p l lcdw;
+  m: 'p m cdw mfree;
+  r: 'p r lcdw
+  > cols
+
+type 'p le = <
+  l: ('p l, b hzip) pull;
+  m: 'p l first mfree;
+  r: ('p m, 'p r) push
+  > cols
+
+type 'p ri = <
+  l: ('p m, 'p l) push;
+  m: 'p r first mfree;
+  r: ('p r, b hzip) pull
+  > cols
+
+type ('f,'b) line = 'f -> 'f -> 'f -> 'b -> 'b -> 'b
+
+type ('a,'b) scol = < l: 'a ;m:'b; r:'a> col
+
+
+type mcol = ((f,b) line, f) scol
+
+type start = (
+  (mcol, b hzip) line,
+  mcol
+) scol
+
+type 'a world = 'world constraint 'a = <world:'world; ..>
+type 'a player = 'world constraint 'a = <world:'world; ..>
+
+
+type 'a p = 'player
+  constraint 'a = <player: 'player; ..>
+type 'a w = 'w
+  constraint 'a = <world: 'w; ..>
+
+
+type ('a,'b, 'c) wmove = 'a -> <world: 'b; player: <left_hand:'lh; right_hand:'rh; back:'c> >
+  constraint 'a = <world:'w; player: <left_hand:'lh; right_hand:'rh; back:'back> >
+
+type 'a event = 'event
+  constraint 'a = < event:'event; ..>
+
+type ('a,'b,'c) eventless = ('a,'b,'c) wmove
+    constraint
+      'a w m m event = false'
+
+
+type 'a left_hand = 'lh constraint 'a = <left_hand:'lh; ..>
+type 'a right_hand = 'x constraint 'a = <right_hand:'x; ..>
+type 'a fl = 'f constraint 'a = <floor:'f Inventory.pickable; ..>
+
+type 'a st = 'f constraint 'a = <stair:'f; ..>
+
+
+
+type 'a pick = <
+  world:
+    <l:'w l; r:'w r;
+     m: <l:'m l;r:'m r;
+         m:free
+        >
+    >;
+  player:<left_hand:'m m fl; right_hand:'a p right_hand; back:Previous.none>
+>
+  constraint 'w = 'a w
+  constraint 'm = 'w m
+
+
+type 'a swap = <
+  world: 'a world;
+  player:<left_hand:'a p right_hand fl; right_hand:'a p left_hand; back:Previous.none>
+>
+
+type 'a open_door = <
+  player:<left_hand:Inventory.empty; right_hand:'a p right_hand; back:Previous.none >;
+  world:<
+    l:'a world l;
+    r: 'a world r;
+    m: <l:'a world m l; m:free; r:'a world m r>;
   >
-constraint 'arg =  <
-    left:'l;
-    player: <left_hand:'lh; right_hand:'rh>;
-    right:'r;
-    at: 'at
-  >
+> constraint
+  'a w m m event = Events.door
+  constraint
+  'a p left_hand = Inventory.key
 
-type winning = Win
-type 'a the_end = winning constraint 'a = <at: stair free hcase; ..>
+type 'a move =
+  | L: ('a, 'a world le, Previous.right ) eventless move
+  | U: ('a, 'a world up, Previous.down ) eventless move
+  | D: ('a, 'a world dw, Previous.up ) eventless move
+  | R: ('a,'a world ri, Previous.left ) eventless move
 
+  | P: ('a -> 'a pick) move
+  | S: ('a -> 'a swap) move
+  | O: ('a -> 'a open_door) move
 
-
-type ('a,'b) move =
-  | L: ('arg, 'arg left) move
-  | R: ('arg, 'arg right) move
-  | T: ('arg, 'arg take) move
-  | S: ('arg, 'arg swap) move
-  | O: ('arg, 'arg open_door) move
-  | End: ('arg, 'arg the_end) move
-
-type _ path =
-  | []: ('a -> 'a) path
-  | (::): ('a,'b) move * ('s->'a) path -> ('s->'b) path
+  | Escape:  (<stair:true'; ..> m m world, won, Previous.none) wmove move
 
 
-let rec (@):type a b c. (b -> c) path -> (a -> b) path -> (a -> c) path =
-  fun l r -> match l with
-  | [] -> r
-  | a :: l -> a :: ( l @ r)
+module type game = sig
+  type start
+  type _ path =
+    | []: start path
+    | (::): ('a -> 'b) move * 'a path -> 'b path
+end
 
-type 'a play = (Start.t -> 'a) path
 
-let play (x: _ play) = x
+let game (type a b c d e f ma mb mc md me mf m mr r)
+    (_: <l:a->b->c->d->e->f; m:<l:ma->mb->mc->md->me->mf;r:mr;m:m>; r: r> Builder.bcols):
+  (module game
+    with type start =
+           <player:player_start;
+            world:<l:a->b->c->d->e->f; m:<l:ma->mb->mc->md->me->mf;r:mr;m:m>; r: r>
+                  >)
+= (module struct
+  type start = <player: player_start; world:<l:a->b->c->d->e->f; m:<l:ma ->mb->mc->md->me-> mf;r:mr;m:m>; r: r> >
+  type _ path =
+    | []: start path
+    | (::): ('a->'b) move * 'a path -> 'b path
+  end)
 
-let s = play []
+(*
+module Level_test = (val game begin
+    [ [K; F; F], F, [F; F; F] ;
+      [F; F; F], F, [F; K; F] ;
+      [F; F; F], F, [F; F; F] ],
+    ( [F; F; F], F, [F; F; F] ),
+    [ [F; K; F], F, [F; F; F] ;
+      [F; F; F], F, [F; F; K] ;
+      [F; F; F], F, [F; F; F] ]
+  end)
 
-let n1 = play [O;R;R;R;R;R;R;R;T;L;T;L;O;L;L;L;L;T;R;R]
-let n2 =   [End; R; O; S; R] @ n1
+module Test = struct
+  open Level_test
+  let _ =[]
+
+
+  let _ = [R;L] = []
+
+  let s = [U;D] = []
+
+
+  let t = [L;L;L;D;R;R;R] = [D]
+  let w = [L;L;L;L;D;D;D;R;R;R]
+
+
+  let s = [U;L] = [L;U]
+  let s = [D;L] = [L;D]
+  let s = [U;R] = [R;U]
+  let s = [D;R] = [R;D]
+
+  let hamiltonian (x: start path) = match x with
+    | [] -> ()
+    | [_] -> .
+    | [_;_] -> ()
+    | [_;_;_] -> .
+    | _ -> ()
+
+end
+*)
+
+module Level0 = struct
+  module G= (val game begin
+    [ [F; F; W], F, [F; F; F] ;
+      [F; F; F], F, [W; W; F] ;
+      [F; F; F], W, [W; W; F] ],
+    ( [W; F; W], F, [K; D; F] ),
+    [ [F; F; W], F, [W; W; W] ;
+      [F; W; W], F, [F; F; W] ;
+      [F; S; W], W, [W; W; W] ]
+  end)
+  open G
+  let s = []
+  let p = [D;O;D;P;D]
+  let path = Escape::D::R::R::U::R::R::R::U::U::R::U::U::U::L::L::L::D::O::D::P::D::[]
+
+end
